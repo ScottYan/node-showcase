d3e252491c88e59c6d88d7a697559ebcba4313e1
Scott Yan <scottyan19@gmail.com>
2016-07-29 09:07:10 +0800

    add ./ to path

--- 01-hello/src/index.js
+++ 01-hello/src/index.js
@@ -3,7 +3,7 @@
  */
 import React from "react";
 import ReactDom from "react-dom";
-import Hello from 'component/Hello'
+import Hello from './component/Hello'
 
 ReactDom.render(
     <Hello msg="Scott"></Hello>,
